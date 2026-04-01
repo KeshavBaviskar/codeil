6f587f7f693f430c8b4e9207ac9fb42ad47e02fb
Keshav Baviskar <kbaviskar787@gmail.com>
2026-04-01 08:30:37 +0530

    small changes

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const express = require('express');
 const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
+
+
 
 app.use(express.static('./assets'));
 
 app.use(expressLayouts);
 
-app.use('layout extractStyles', true);
-app.use('layout extractScripts', true);
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 app.use('/', require('./routes/index'));
 app.set('view engine', 'ejs');
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,10 @@
-const express=require('express');
+const express = require('express');
 
-const router=express.Router();
-const homeController=require('../controllers/home_controller');
+const router = express.Router();
+const homeController = require('../controllers/home_controller');
 console.log('router loaded');
 
-router.get('/',homeController.home);
-router.use('/users',require('./users'));
+router.get('/', homeController.home);
+router.use('/users', require('./users'));
 
-module.exports=router;
-  
+module.exports = router;
