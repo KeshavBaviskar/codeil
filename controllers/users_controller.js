4a4db3c627ef751f93cc8e02bd118f0d89967bb8
Keshav Baviskar <kbaviskar787@gmail.com>
2026-04-02 17:49:06 +0530

    created signup code

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 module.exports.profile = function (req, res) {
     return res.render('user_profile', {
         title: "Home"
@@ -10,16 +12,32 @@ module.exports.signUp = function (req, res) {
     })
 }
 
-module.exports.signIn=function(req,res){
-     return res.render('user_sign_in', {
+module.exports.signIn = function (req, res) {
+    return res.render('user_sign_in', {
         title: "Codeial | Sign In"
     })
 }
 
-module.exports.create=function(req,res){
-         
+module.exports.create = async function (req, res) {
+    if (req.body.password != req.body.confirm_password) {
+        return res.redirect('back');
+    }
+
+    try {
+        const user = await User.findOne({ email: req.body.email });
+
+        if (!user) {
+            await User.create(req.body);
+            return res.redirect('/users/sign-in');
+        }
+
+        return res.redirect('back');
+    } catch (err) {
+        console.log(`error in signing up user: ${err}`);
+        return res.redirect('back');
+    }
 }
 
-module.exports.createSession=function(req,res){
-         
+module.exports.createSession = function (req, res) {
+
 }
