ef287a68d988c171057cc860e50d7e7c6f47fff7
Keshav Baviskar <kbaviskar787@gmail.com>
2026-04-02 12:34:31 +0530

    created sign in and sign up pages

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,25 @@
-module.exports.profile=function(req,res){
-        return res.render('user_profile',{
-        title:"Home"
+module.exports.profile = function (req, res) {
+    return res.render('user_profile', {
+        title: "Home"
     })
-} 
+}
+
+module.exports.signUp = function (req, res) {
+    return res.render('user_sign_up', {
+        title: "Codeial | Sign Up"
+    })
+}
+
+module.exports.signIn=function(req,res){
+     return res.render('user_sign_in', {
+        title: "Codeial | Sign In"
+    })
+}
+
+module.exports.create=function(req,res){
+         
+}
+
+module.exports.createSession=function(req,res){
+         
+}
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,11 @@
-const express=require('express');
-const router=express.Router();
+const express = require('express');
+const router = express.Router();
 
-const usersController=require('../controllers/users_controller');
+const usersController = require('../controllers/users_controller');
 
-router.get('/profile',usersController.profile);
+router.get('/profile', usersController.profile);
 
-module.exports=router;
+router.get('/sign-up', usersController.signUp);
+router.get('/sign-in', usersController.signIn);
+
+module.exports = router;
